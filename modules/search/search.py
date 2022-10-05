05df055c12ed48bd2ae51798f1f2fc4ae00ac054
Nyan Lin <mathnyanlin@gmail.com>
2022-10-05 11:15:44 +0630

    adding Arc

--- modules/search/search.py
+++ modules/search/search.py
@@ -1,3 +1,4 @@
+from asyncio import constants
 import random
 
 class Agent(object):
@@ -23,5 +24,22 @@ class TP_env():
         self.stock_history.append(self.stock)
         price = self.prices[0]+random.randrange(self.max_price_addon)
         return price
+
+class Arc(object):
+    def __init__(self, from_node, to_node, cost=1, action=None):
+        self.from_node = from_node
+        self.to_node = to_node
+        self.cost = cost
+        self.action = action
+
+    def __repr__(self):
+        if self.action:
+            return str(self.from_node)+'--'+str(self.action)+'---'+str(self.to_node)
+        else:
+            return str(self.from_node)+'-->'+str(self.to_node)
+
 tp = TP_env()
 print(tp.initial_precepts())
+arc = [Arc('a','b',1),Arc('b','c',1),Arc('b','d',1)]
+for i in arc:
+    print(i)
