eef0350d680a858690bc15956b7ccc1f18f8e39e
Nyan Lin <mathnyanlin@gmail.com>
2022-10-07 17:36:24 +0630

    file updateing

--- maze.py
+++ maze.py
@@ -1,21 +1,41 @@
 import sys
 
 class Node(object):
-    def __init__(self, start, board, goal={}):
+    """
+    node properties
+    """
+    def __init__(self, start, goal, board):
         self.start = start
-        self.goal = goal
         self.board = board
+        self.goal = goal
 
-    def get_direction(self):
-        left = self.start + 1
-        right = self.start - 1
-        return left
+    def go(self):
+        return self.start
 
-    def go():
-        pass
+    def way_decision(self, i_start):
+        """
+        to find four passible direction
+        """
+        path_ways = [-1, 1]
+        def find_ways(ind):
+            if self.board[ind] == ' ':
+                pass
+            else:
+                return find_ways(ind-1)
+        return self.board[i_start]
 
     def is_goal(self):
         return self.start
+
+class Graph(object):
+    def __init__(self):
+        self.graph = []
+
+    def add_node(self, node):
+        self.graph[node] = []
+
+    def remove_node(self, node):
+        pass
 
 class StackFrontier(object):
     """
@@ -35,25 +55,45 @@ class StackFrontier(object):
             self.frontier = self.frontier[1:]
             return self.frontier
 
-def search_node(move_node ,start_node, end_node, c=0):
+def search_node(move_node ,node, c=0):
     """
-    produce: start_node and goal_node's index
+    produce: node index
     """
     if len(move_node)>=1:
-        if move_node[0] == start_node:
+        if move_node[0] == node:
             return c
         else:
             c += 1
-            return search_node(move_node[1:], start_node, end_node, c=c)
+            return search_node(move_node[1:], node, c=c)
     else:
         return Exception("no node hasn't")
 
-def main():
-    board = ''
+def collect_string(board):
+    result = ''
+    for i in board:
+        result += i
+    return result
+
+def main(start_node, goal_node):
+
+    board = []
     with open(sys.argv[1],'rt') as f:
-        board +=f.read()
+        result = ''
+        for i in f.read():
+            if i == '\n':
+                board.append(result)
+                result = ''
+            else:
+                result += i
     f.close()
-    return print(search_node(board,'A','B'))
+    string_board = collect_string(board)
+    i_start = search_node(string_board, start_node)
+    i_goal = search_node(string_board, goal_node)
+
+    n = Node(start_node, goal_node, string_board)
+    print(n.way_decision(i_start))
+    # print(len(board))
+    # nodes = {start_node:i_start,goal_node:i_goal}
 
 if __name__=='__main__':
-    main()
+    main('A','B')
