bb033837d029744da5bc6003201b579c36b14f66
Nyan Lin <mathnyanlin@gmail.com>
2022-10-19 20:53:05 +0630

    adding breath_ndex_node

--- maze/extract_file.py
+++ maze/extract_file.py
@@ -20,9 +20,27 @@ def index_node(board, node):
         for j in i:
             col += 1
             if j == node:
-                return (row, col)
+                return [row, col]
         col = -1
-                
+
+
+def breath_next_node(board, node_index, count = 0):
+    """ produce next node """
+    possible_node_list = [
+        [node_index[0],node_index[1]+1],
+        [node_index[0],node_index[1]-1],
+        [node_index[0]+1,node_index[1]],
+        [node_index[0]-1,node_index[1]]
+    ]
+    if count < 4:
+        single_node = possible_node_list[count]
+        if board[single_node[0]][single_node[1]] == ' ':
+            return single_node
+        else:
+            return breath_next_node(board, node_index, count = count +1)
+    else:
+        return 'did not found anything'
+
 
 
 
--- maze/maze.py
+++ maze/maze.py
@@ -1,6 +1,8 @@
-from extract_file import produce_board, index_node
+from extract_file import produce_board, index_node, breath_next_node
 import sys
 
 board = produce_board(sys.argv[1])
 index_node = index_node(board, 'A')
+
+print(breath_next_node(board, index_node))
 
